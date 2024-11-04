99622f4e7955ce356c350596a8917a3834f75854
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-04 15:21:53 -0300

    Update Falabella

--- storescraper/stores/falabella.py
+++ storescraper/stores/falabella.py
@@ -1039,13 +1039,18 @@ class Falabella(Store):
                     raise Exception("No slides found")
 
                 for idx, slide in enumerate(slides):
+                    if "type" not in slide:
+                        continue
+
                     main_url = slide.get("mainUrl", None)
+
                     if main_url:
                         destination_urls = [main_url[:500]]
                     elif slide["type"] == "background_image_only":
                         destination_urls = []
                     else:
                         destination_urls = list({slide["urlLeft"], slide["urlRight"]})
+                        
                     picture_url = slide["imgBackgroundDesktopUrl"]
 
                     banners.append(
