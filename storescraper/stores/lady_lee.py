a2ee212e7b9fff572b69c9f0c1ecd167926201ca
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-07 11:38:42 -0300

    Update LadyLee

--- storescraper/stores/lady_lee.py
+++ storescraper/stores/lady_lee.py
@@ -4,22 +4,18 @@ import json
 from storescraper.categories import TELEVISION
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy, html_to_markdown
+from storescraper.utils import session_with_proxy, html_to_markdown, remove_words
 
 
 class LadyLee(Store):
 
     @classmethod
     def categories(cls):
-        return [
-            TELEVISION
-        ]
+        return [TELEVISION]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            TELEVISION
-        ]
+        url_extensions = [TELEVISION]
 
         session = session_with_proxy(extra_args)
         product_urls = []
@@ -30,23 +26,25 @@ class LadyLee(Store):
             page = 0
             while True:
                 if page > 10:
-                    raise Exception('Page overflow')
+                    raise Exception("Page overflow")
 
-                url_webpage = 'https://api.c8gqzlqont-mantenimi1-p1-public.m' \
-                    'odel-t.cc.commerce.ondemand.com/occ/v2/myshop-spa/produ' \
-                    'cts/search?query=LG:relevance:brand:LG&pageSize=100&cur' \
-                    'rentPoS=D001&currentPage={}'.format(page)
+                url_webpage = (
+                    "https://api.c8gqzlqont-mantenimi1-p1-public.m"
+                    "odel-t.cc.commerce.ondemand.com/occ/v2/myshop-spa/produ"
+                    "cts/search?query=LG:relevance:brand:LG&pageSize=100&cur"
+                    "rentPoS=D001&currentPage={}".format(page)
+                )
 
                 data = session.get(url_webpage).text
-                json_data = json.loads(data)['products']
+                json_data = json.loads(data)["products"]
 
                 if len(json_data) == 0:
                     if page == 0:
-                        raise Exception('Empty category: ' + url_webpage)
+                        raise Exception("Empty category: " + url_webpage)
                     break
 
                 for product in json_data:
-                    product_urls.append('https://ladylee.net' + product['url'])
+                    product_urls.append("https://ladylee.net" + product["url"])
 
                 page += 1
 
@@ -56,10 +54,12 @@ class LadyLee(Store):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        sku = url.split('/')[4]
-        product_url = 'https://api.c8gqzlqont-mantenimi1-p1-public.model-t.c' \
-            'c.commerce.ondemand.com/occ/v2/myshop-spa/products/{}?fields=DE' \
-            'FAULT,images(FULL,galleryIndex),ean&currentPoS=D001'.format(sku)
+        sku = url.split("/")[4]
+        product_url = (
+            "https://api.c8gqzlqont-mantenimi1-p1-public.model-t.c"
+            "c.commerce.ondemand.com/occ/v2/myshop-spa/products/{}?fields=DE"
+            "FAULT,images(FULL,galleryIndex),ean&currentPoS=D001".format(sku)
+        )
         response = session.get(product_url, allow_redirects=False)
 
         if response.status_code == 400:
@@ -68,17 +68,18 @@ class LadyLee(Store):
 
         json_data = json.loads(response.text)
 
-        assert sku == json_data['code']
+        assert sku == json_data["code"]
 
-        description = json_data['description']
-        name = json_data['name']
-        price = Decimal(json_data['price']['value'])
-        if json_data['availableForPickup']:
+        description = json_data["description"]
+        name = json_data["name"]
+        price = Decimal(remove_words(json_data["price"]["formattedValue"], ["L.", ","]))
+
+        if json_data["availableForPickup"]:
             stock = -1
         else:
             stock = 0
 
-        picture_urls = [json_data['images'][0]['url']]
+        picture_urls = [json_data["images"][0]["url"]]
 
         p = Product(
             name,
@@ -90,10 +91,10 @@ class LadyLee(Store):
             stock,
             price,
             price,
-            'HNL',
+            "HNL",
             sku=sku,
             picture_urls=picture_urls,
-            description=description
+            description=description,
         )
 
         return [p]
