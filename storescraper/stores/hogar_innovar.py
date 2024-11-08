851a995ea3c558d6dc63e723028c6425ad7eecda
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-08 12:26:55 -0300

    update HogarInnovar

--- storescraper/stores/hogar_innovar.py
+++ storescraper/stores/hogar_innovar.py
@@ -1,4 +1,5 @@
 import json
+import re
 from decimal import Decimal
 
 from bs4 import BeautifulSoup
@@ -23,25 +24,20 @@ class HogarInnovar(Store):
         page = 1
 
         while True:
-            if page >= 15:
+            if page >= 10:
                 raise Exception("Page overflow")
 
-            url = "https://hogarinnovar.com/shop-3/page/{}/?s=LG&post_type=product&product_brand=lg".format(
-                page
-            )
+            url = f"https://europe-west1-rapid-product-search.cloudfunctions.net/appV2/api/e5b998-78.myshopify.com/search/?data_index=es_sy4_free_9&engine=shopify&query_id=f08e17eb-014e-4a40-8813-ef937bb13562/1731076020302&max_product_count=30&page={page}&type=resultPage&filter_category_id=296865398895"
             print(url)
 
-            soup = BeautifulSoup(session.get(url).text, "lxml")
-            products_container = soup.findAll("li", "product")
+            response = session.get(url).json()
+            products = response["products"]
 
-            if not products_container:
-                if page == 1:
-                    raise Exception("Empty site: {}".format(url))
+            if not products:
                 break
 
-            for product in products_container:
-                product_url = product.find("a")["href"]
-                product_urls.append(product_url)
+            for product in products:
+                product_urls.append(f"https://hogarinnovar.com/{product['productUrl']}")
 
             page += 1
 
@@ -53,26 +49,32 @@ class HogarInnovar(Store):
         session = session_with_proxy(extra_args)
         data = session.get(url).text
         soup = BeautifulSoup(data, "lxml")
-        key = soup.find("link", {"rel": "shortlink"})["href"].split("?p=")[-1]
-
         product_data = json.loads(
-            soup.findAll("script", {"type": "application/ld+json"})[-1].text
-        )["@graph"][0]
-        name = product_data["name"]
-        sku = product_data.get("sku", None)
+            soup.findAll("script", {"type": "application/ld+json"})[1].text
+        )
+        variants = product_data["hasVariant"]
 
-        if "offers" not in product_data:
+        assert len(variants) == 1
+
+        variant = variants[0]
+
+        name = variant["name"]
+        offer = variant["offers"]
+        price = Decimal(offer["price"])
+
+        if price == 0:
             return []
 
-        offer = product_data["offers"][0]
-        if offer["availability"] == "http://schema.org/InStock":
-            stock = -1
-        else:
-            stock = 0
+        stock = -1 if offer["availability"] == "http://schema.org/InStock" else 0
 
-        price = Decimal(offer["priceSpecification"]["price"])
-        description = product_data["description"]
-        picture_urls = [x["data-src"] for x in soup.findAll("img", "owl-lazy")]
+        regex = r"/products/([^?]+)\?variant=(\d+)"
+        match = re.search(regex, offer["url"])
+        sku = match.group(1)
+        key = match.group(2)
+        picture_urls = [
+            f"https:{img['src'].split('?')[0]}"
+            for img in soup.find("product-modal", "product-media-modal").findAll("img")
+        ]
 
         p = Product(
             name,
@@ -86,8 +88,8 @@ class HogarInnovar(Store):
             price,
             "COP",
             sku=sku,
+            part_number=sku,
             picture_urls=picture_urls,
-            description=description,
         )
 
         return [p]
