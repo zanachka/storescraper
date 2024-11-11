c9f92c408106fb26d99812ef3821a052e8f47fe1
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-11 10:37:09 -0300

    Update categories ram comment

--- storescraper/categories.py
+++ storescraper/categories.py
@@ -3,120 +3,120 @@
 #########################
 
 # Aire acondicionado (no purificadores ni ventiladores)
-AIR_CONDITIONER = 'AirConditioner'
+AIR_CONDITIONER = "AirConditioner"
 # All in one (equipos tipo iMac, no desktops sin monitor o pc armados)
-ALL_IN_ONE = 'AllInOne'
+ALL_IN_ONE = "AllInOne"
 # Audifonos de todos los tipos
-HEADPHONES = 'Headphones'
+HEADPHONES = "Headphones"
 # Celulares
-CELL = 'Cell'
+CELL = "Cell"
 # Combos teclado mouse, opcionalmente con audifonos u otros accesorios
-KEYBOARD_MOUSE_COMBO = 'KeyboardMouseCombo'
+KEYBOARD_MOUSE_COMBO = "KeyboardMouseCombo"
 # Consolas (solo oficiales)
-VIDEO_GAME_CONSOLE = 'VideoGameConsole'
+VIDEO_GAME_CONSOLE = "VideoGameConsole"
 # Cooler de procesador (no ventiladores sueltos)
-CPU_COOLER = 'CpuCooler'
+CPU_COOLER = "CpuCooler"
 # Discos duros de desktop o notebook (no SSDs ni discos de servidor)
-STORAGE_DRIVE = 'StorageDrive'
+STORAGE_DRIVE = "StorageDrive"
 # Discos duros externos (usualmente USB)
-EXTERNAL_STORAGE_DRIVE = 'ExternalStorageDrive'
+EXTERNAL_STORAGE_DRIVE = "ExternalStorageDrive"
 # Equipos de audio en general (soundtower, soundbar, etc)
-STEREO_SYSTEM = 'StereoSystem'
+STEREO_SYSTEM = "StereoSystem"
 # Estufas de todos tipo (electricas, parafina, etc)
-SPACE_HEATER = 'SpaceHeater'
+SPACE_HEATER = "SpaceHeater"
 # Pendrives (USB / USB-C, etc)
-USB_FLASH_DRIVE = 'UsbFlashDrive'
+USB_FLASH_DRIVE = "UsbFlashDrive"
 # Fuentes de poder de PC desktop (no servidores)
-POWER_SUPPLY = 'PowerSupply'
+POWER_SUPPLY = "PowerSupply"
 # Gabinetes de PC Desktop
-COMPUTER_CASE = 'ComputerCase'
+COMPUTER_CASE = "ComputerCase"
 # Hornos y microondas
-OVEN = 'Oven'
+OVEN = "Oven"
 # Impresoras (solo de casa / oficina, no térmicas)
-PRINTER = 'Printer'
+PRINTER = "Printer"
 # Lavadoras y secadoras de todos tipo
-WASHING_MACHINE = 'WashingMachine'
+WASHING_MACHINE = "WashingMachine"
 # Monitores de PC desktop (no all in one ni pantallas de anuncios)
-MONITOR = 'Monitor'
+MONITOR = "Monitor"
 # Mouse
-MOUSE = 'Mouse'
+MOUSE = "Mouse"
 # Notebooks
-NOTEBOOK = 'Notebook'
+NOTEBOOK = "Notebook"
 # Placas madres de pc desktop (no servidores)
-MOTHERBOARD = 'Motherboard'
+MOTHERBOARD = "Motherboard"
 # Planes de telefonía
-CELL_PLAN = 'CellPlan'
+CELL_PLAN = "CellPlan"
 # Procesadores de PC Desktop (no de notebook ni de servidor)
-PROCESSOR = 'Processor'
-# RAM de PC desktop o notebook (no servidores)
-RAM = 'Ram'
+PROCESSOR = "Processor"
+# RAM de PC desktop o notebook
+RAM = "Ram"
 # Refrigeradores en general (incluye freezers)
-REFRIGERATOR = 'Refrigerator'
+REFRIGERATOR = "Refrigerator"
 # Tablets Android / iOS (no incluye e-readers como los kindle)
-TABLET = 'Tablet'
+TABLET = "Tablet"
 # Tarjetas de memoria SD / microSD / CompactFlash
-MEMORY_CARD = 'MemoryCard'
+MEMORY_CARD = "MemoryCard"
 # Tarjetas de video para PC desktop
-VIDEO_CARD = 'VideoCard'
+VIDEO_CARD = "VideoCard"
 # Teclados
-KEYBOARD = 'Keyboard'
+KEYBOARD = "Keyboard"
 # Televisores (no monitores)
-TELEVISION = 'Television'
+TELEVISION = "Television"
 # Unidades de estado solido (SSD) de 2.5" o M.2 para desktop o notebook
-SOLID_STATE_DRIVE = 'SolidStateDrive'
+SOLID_STATE_DRIVE = "SolidStateDrive"
 # UPS (solo con bateria, no reguladores de voltaje)
-UPS = 'Ups'
+UPS = "Ups"
 # Wearables (bandas deportivas o smartwatch)
-WEARABLE = 'Wearable'
+WEARABLE = "Wearable"
 # Sillas gamer (no de oficina)
-GAMING_CHAIR = 'GamingChair'
+GAMING_CHAIR = "GamingChair"
 # Microfonos
-MICROPHONE = 'Microphone'
+MICROPHONE = "Microphone"
 # Escritorios gamer (no de oficina)
-GAMING_DESK = 'GamingDesk'
+GAMING_DESK = "GamingDesk"
 # Ventiladores para gabinetes de PC desktop
-CASE_FAN = 'CaseFan'
+CASE_FAN = "CaseFan"
 # Abarrotes
-GROCERIES = 'Groceries'
+GROCERIES = "Groceries"
 
 #########################
 # Legacy (no usar)
 #########################
 
 # Lectores de DVD / Blu-ray para PCs desktop
-OPTICAL_DRIVE = 'OpticalDrive'
+OPTICAL_DRIVE = "OpticalDrive"
 # Tubos de iluminación LED
-LIGHT_TUBE = 'LightTube'
+LIGHT_TUBE = "LightTube"
 # Proyectores de luz LED
-LIGHT_PROJECTOR = 'LightProjector'
+LIGHT_PROJECTOR = "LightProjector"
 # Juegos de video
-VIDEO_GAME = 'VideoGame'
+VIDEO_GAME = "VideoGame"
 # Camaras profesionales
-D_S_L_R = 'DSLR'
+D_S_L_R = "DSLR"
 # Ampolletas
-LAMP = 'Lamp'
+LAMP = "Lamp"
 # Camaras de video
-VIDEO_CAMERA = 'VideoCamera'
+VIDEO_CAMERA = "VideoCamera"
 # Camaras digitales de fotografia
-CAMERA = 'Camera'
+CAMERA = "Camera"
 # Cooler de tarjetas de video
-VGA_COOLER = 'VgaCooler'
+VGA_COOLER = "VgaCooler"
 # Aspiradoras
-VACUUM_CLEANER = 'VacuumCleaner'
+VACUUM_CLEANER = "VacuumCleaner"
 # Calefones
-WATER_HEATER = 'WaterHeater'
+WATER_HEATER = "WaterHeater"
 
 #########################
 # Reservados (no usar normalmente)
 #########################
 
 # Reproductores de DVD y Blu-ray para televisores
-OPTICAL_DISK_PLAYER = 'OpticalDiskPlayer'
+OPTICAL_DISK_PLAYER = "OpticalDiskPlayer"
 # Proyectores (como para hacer presentaciones)
-PROJECTOR = 'Projector'
+PROJECTOR = "Projector"
 # Lavavajillas
-DISH_WASHER = 'DishWasher'
+DISH_WASHER = "DishWasher"
 # Accesorios de celular (carcasas, cables)
-CELL_ACCESORY = 'CellAccesory'
+CELL_ACCESORY = "CellAccesory"
 # Cocinas
-STOVE = 'Stove'
+STOVE = "Stove"
