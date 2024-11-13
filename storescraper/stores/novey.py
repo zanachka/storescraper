32c59f57ff8e4b4fabd56bbb19f28fc4f4942449
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-13 12:05:22 -0300

    Update Novey

--- storescraper/stores/novey.py
+++ storescraper/stores/novey.py
@@ -34,7 +34,7 @@ class Novey(Store):
             ]
         }
         session.headers = {
-            "x-algolia-api-key": "YWI4NmJlZTZkZjg2NzBlNzFjYjYwYTE5MjI4MjI1ZTNiY2ZiYTI0Y2RjODY5OWMwMzU3NDI2ZDk1YTFjZTgwZWZpbHRlcnM9Y2F0YWxvZ19wZXJtaXNzaW9ucy5jdXN0b21lcl9ncm91cF8wJTIwJTIxJTNEJTIwMCZ0YWdGaWx0ZXJzPSZ2YWxpZFVudGlsPTE3MzE0MTc2Mjk=",
+            "x-algolia-api-key": "ZDEzNjk3ZGNhZjk5NmEyMDE0OWQ4ODc5MTZmZDJiMDU5MDA3ODNmNWM3MTdmZjA5N2I1YjMxNGM1ODM4NjA0NGZpbHRlcnM9Y2F0YWxvZ19wZXJtaXNzaW9ucy5jdXN0b21lcl9ncm91cF8wJTIwJTIxJTNEJTIwMCZ0YWdGaWx0ZXJzPSZ2YWxpZFVudGlsPTE3MzE1OTI2MjU=",
             "x-algolia-application-id": "ZCZRBTYD8I",
         }
         page = 0
