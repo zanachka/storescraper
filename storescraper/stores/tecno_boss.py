1a0002b924f22c4c556409e9a31c178341697179
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-19 11:50:50 -0300

    Update TecnoBoss

--- storescraper/stores/tecno_boss.py
+++ storescraper/stores/tecno_boss.py
@@ -1,5 +1,6 @@
 from decimal import Decimal
 import logging
+import json
 from bs4 import BeautifulSoup
 
 from storescraper.categories import (
@@ -9,25 +10,26 @@ from storescraper.categories import (
     RAM,
     SOLID_STATE_DRIVE,
     VIDEO_CARD,
-    STEREO_SYSTEM,
     PRINTER,
+    NOTEBOOK,
+    POWER_SUPPLY,
 )
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
-from storescraper.utils import remove_words, session_with_proxy
+from storescraper.utils import session_with_proxy
 
 
 class TecnoBoss(StoreWithUrlExtensions):
     url_extensions = [
-        ["audifonos", HEADPHONES],
-        ["smart-home", STEREO_SYSTEM],
-        ["memorias-ram", RAM],
-        ["ssd", SOLID_STATE_DRIVE],
-        ["home-office", MONITOR],
-        ["tarjetas-de-video", VIDEO_CARD],
+        ["notebooks", NOTEBOOK],
+        ["todos-los-productos-1/monitor", MONITOR],
         ["mouse-y-teclados", MOUSE],
-        ["computadores", MONITOR],
-        ["impresoras", PRINTER],
+        ["ssd", SOLID_STATE_DRIVE],
+        ["memorias-ram", RAM],
+        ["tarjetas-de-video", VIDEO_CARD],
+        ["audifonos", HEADPHONES],
+        ["todos-los-productos-1/impresoras", PRINTER],
+        ["todos-los-productos-1/fuente-de-poder", POWER_SUPPLY],
     ]
 
     @classmethod
@@ -35,24 +37,28 @@ class TecnoBoss(StoreWithUrlExtensions):
         session = session_with_proxy(extra_args)
         product_urls = []
         page = 1
+
         while True:
             if page > 10:
-                raise Exception("Page overflow: " + url_extension)
-            url_webpage = "https://www.tecnoboss.cl/{}" "?page={}".format(
-                url_extension, page
-            )
+                raise Exception(f"Page overflow: {url_extension}")
+            url_webpage = f"https://www.tecnoboss.cl/{url_extension}?page={page}"
             print(url_webpage)
+
             data = session.get(url_webpage).text
             soup = BeautifulSoup(data, "lxml")
-            product_containers = soup.findAll("div", "product-block")
+            product_containers = soup.findAll("article", "product-block")
+
             if not product_containers:
                 if page == 1:
-                    logging.warning("Empty category: " + url_extension)
+                    logging.warning(f"Empty category: {url_extension}")
                 break
+
             for container in product_containers:
-                product_url = container.find("a", "product-image")["href"]
-                product_urls.append("https://www.tecnoboss.cl" + product_url)
+                product_url = container.find("a")["href"]
+                product_urls.append(f"https://www.tecnoboss.cl{product_url}")
+
             page += 1
+
         return product_urls
 
     @classmethod
@@ -61,37 +67,18 @@ class TecnoBoss(StoreWithUrlExtensions):
         session = session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, "lxml")
+        product_data = json.loads(soup.find("script", "product-form-json").text)["info"]
+        variant = product_data["variant"]
 
-        key = soup.find("form", {"name": "buy"})["action"].split("/")[-1]
-
-        name = soup.find("meta", {"property": "og:title"})["content"]
+        name = product_data["product"]["name"]
+        key = str(variant["id"])
+        stock = variant["stock"]
+        price = Decimal(variant["price_discount"])
         description = soup.find("meta", {"property": "og:description"})["content"]
-
-        price = Decimal(remove_words(soup.find("span", "product-form-price").text))
-
-        if not price:
-            return []
-
-        sku_span = soup.find("span", "sku_elem")
-        if sku_span and sku_span.text != "":
-            sku = sku_span.text
-        else:
-            sku = None
-
-        stock_div = soup.find("div", "product-page_stock")
-        if stock_div:
-            stock = int(stock_div.find("span").text)
-        else:
-            stock = 0
-
-        picture_urls = []
-        picture_container = soup.find("div", "carousel-inner")
-        if picture_container:
-            for image in picture_container.findAll("img"):
-                picture_urls.append(image["src"])
-        else:
-            image = soup.find("div", "main-product-image").find("img")
-            picture_urls.append(image["src"])
+        picture_urls = [
+            picture.find("img")["src"]
+            for picture in soup.findAll("picture", "product-gallery__picture")
+        ]
 
         p = Product(
             name,
@@ -104,7 +91,6 @@ class TecnoBoss(StoreWithUrlExtensions):
             price,
             price,
             "CLP",
-            sku=sku,
             picture_urls=picture_urls,
             description=description,
         )
