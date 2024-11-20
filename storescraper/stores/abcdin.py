e4b3698ca09a9dcbf6bd8c4e4991f9dfc396c8d2
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-20 11:26:31 -0300

    Update AbcDin

--- storescraper/stores/abcdin.py
+++ storescraper/stores/abcdin.py
@@ -433,7 +433,7 @@ class AbcDin(Store):
         session = session_with_proxy(extra_args)
         response = session.get(url)
 
-        if response.status_code == 410:
+        if response.status_code in [410, 404]:
             return []
 
         soup = BeautifulSoup(response.text, "lxml")
