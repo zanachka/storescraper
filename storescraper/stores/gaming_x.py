ccdafe4cf6939381a38e4c296955dd40a24c645d
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-20 10:46:15 -0300

    Update GamingX

--- storescraper/stores/gaming_x.py
+++ storescraper/stores/gaming_x.py
@@ -1,7 +1,7 @@
 from decimal import Decimal
 import logging
 from bs4 import BeautifulSoup
-from storescraper.categories import VIDEO_CARD
+from storescraper.categories import NOTEBOOK
 from storescraper.stores.mercado_libre_chile import MercadoLibreChile
 from storescraper.utils import session_with_proxy
 
@@ -9,12 +9,12 @@ from storescraper.utils import session_with_proxy
 class GamingX(MercadoLibreChile):
     @classmethod
     def categories(cls):
-        return [VIDEO_CARD]
+        return [NOTEBOOK]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [
-            ["computacion", VIDEO_CARD],
+            ["computacion", NOTEBOOK],
         ]
 
         session = session_with_proxy(extra_args)
@@ -22,26 +22,29 @@ class GamingX(MercadoLibreChile):
         for url_extension, local_category in url_extensions:
             if local_category != category:
                 continue
+
             page = 1
+
             while True:
                 if page > 10:
-                    raise Exception("Page overflow: " + url_extension)
+                    raise Exception(f"Page overflow: {url_extension}")
+
                 index = str(50 * (page - 1) + 1)
-                url_webpage = (
-                    "https://www.gamingx.cl/listado/"
-                    "{}/_Desde_{}_NoIndex_True".format(url_extension, index)
-                )
+                url_webpage = f"https://www.gamingx.cl/listado/{url_extension}/_Desde_{index}_NoIndex_True"
                 data = session.get(url_webpage).text
                 soup = BeautifulSoup(data, "lxml")
                 product_containers = soup.findAll("li", "ui-search-layout__item")
+
                 if not product_containers:
                     if page == 1:
-                        logging.warning("Empty category: " + url_extension)
+                        logging.warning(f"Empty category: {url_extension}")
                     break
+
                 for container in product_containers:
-                    product_url = container.find("a", "ui-search-link")["href"]
+                    product_url = container.find("a", "poly-component__title")["href"]
                     product_urls.append(product_url)
                 page += 1
+
         return product_urls
 
     @classmethod
