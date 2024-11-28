14283ea5c906f640f9322ca73a3fc06a38db0919
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-11-28 11:54:19 -0300

    Update Spider

--- storescraper/stores/spider.py
+++ storescraper/stores/spider.py
@@ -96,6 +96,10 @@ class Spider(StoreWithUrlExtensions):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
+
+        if response.status_code == 404:
+            return []
+
         soup = BeautifulSoup(response.text, "lxml")
 
         part_number = (
