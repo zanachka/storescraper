c1035afa6905b9380098a0f8a9f1d5be2c74438d
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-12-03 17:11:19 -0300

    Update TecnoMaster

--- storescraper/stores/tecno_master.py
+++ storescraper/stores/tecno_master.py
@@ -33,7 +33,7 @@ from storescraper.categories import (
 )
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy, remove_words
+from storescraper.utils import html_to_markdown, session_with_proxy, remove_words
 
 
 class TecnoMaster(Store):
@@ -183,6 +183,8 @@ class TecnoMaster(Store):
             tag["src"]
             for tag in soup.find("div", "elementor-widget-image").findAll("img")
         ]
+        description = html_to_markdown(soup.find("div", {"id": "tab-description"}).text)
+
         p = Product(
             name,
             cls.__name__,
@@ -196,5 +198,6 @@ class TecnoMaster(Store):
             "CLP",
             sku=sku,
             picture_urls=picture_urls,
+            description=description,
         )
         return [p]
