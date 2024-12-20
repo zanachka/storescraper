0ca79d7d65e60798f25e2de28ec0eaee5cba1415
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2024-12-20 12:57:13 -0300

    Update Ripley

--- storescraper/stores/ripley.py
+++ storescraper/stores/ripley.py
@@ -355,7 +355,7 @@ class Ripley(Store):
             section_index = 1
 
             while True:
-                if page > 400:
+                if page > 500:
                     raise Exception(f"Page overflow: {category_path}")
 
                 separator = "&" if "?" in category_path else "?"
