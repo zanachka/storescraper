6a4a0bca5f97874bb9b620dccf0c469d8e7d38d2
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-01-15 11:04:55 -0300

    Update KDTec

--- storescraper/stores/k_d_tec.py
+++ storescraper/stores/k_d_tec.py
@@ -163,6 +163,10 @@ class KDTec(StoreWithUrlExtensions):
         sku = product_data.get("sku", None)
         description = product_data["description"]
         price_tags = soup.findAll("span", "woocommerce-Price-amount")
+
+        if not price_tags:
+            return []
+
         assert len(price_tags) == 2
 
         offer_price = Decimal(remove_words(price_tags[0].text))
