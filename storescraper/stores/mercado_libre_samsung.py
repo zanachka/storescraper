ba50156a5a4c3299efcf749a8d8848fe58ec9292
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-03-07 11:42:50 -0300

    Update MercadoLibreSamsung

--- storescraper/stores/mercado_libre_samsung.py
+++ storescraper/stores/mercado_libre_samsung.py
@@ -1,7 +1,20 @@
 from .mercado_libre_chile import MercadoLibreChile
-from storescraper.categories import STEREO_SYSTEM, TELEVISION, CELL_ACCESORY, \
-    CELL, WEARABLE, VACUUM_CLEANER, REFRIGERATOR, AIR_CONDITIONER, OVEN, \
-    WASHING_MACHINE, TABLET, DISH_WASHER, MONITOR, PROJECTOR
+from storescraper.categories import (
+    STEREO_SYSTEM,
+    TELEVISION,
+    CELL_ACCESORY,
+    CELL,
+    WEARABLE,
+    VACUUM_CLEANER,
+    REFRIGERATOR,
+    AIR_CONDITIONER,
+    OVEN,
+    WASHING_MACHINE,
+    TABLET,
+    DISH_WASHER,
+    MONITOR,
+    PROJECTOR,
+)
 from ..utils import session_with_proxy
 
 
@@ -25,60 +38,67 @@ class MercadoLibreSamsung(MercadoLibreChile):
             TABLET,
             PROJECTOR,
             DISH_WASHER,
-            MONITOR
+            MONITOR,
         ]
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
         categories_codes = {
-            STEREO_SYSTEM: ['MLC1010'],  # Audio
-            TELEVISION: ['MLC1002'],  # Televisores
+            STEREO_SYSTEM: ["MLC1010"],  # Audio
+            TELEVISION: ["MLC1002"],  # Televisores
             CELL_ACCESORY: [
-                'MLC3813',  # Accesorios para Celulares
-                'MLC431414',  # Accesorios para TV
-                'MLC179816',  # Repuestos y Accesorios (Refrigeración)
-                'MLC174295',  # Extractores y Purificadores
-                'MLC176937',  # Repuestos y Accesorios (Climatización)
-                'MLC85756',  # Accesorios (Tablet)
+                "MLC3813",  # Accesorios para Celulares
+                "MLC431414",  # Accesorios para TV
+                "MLC179816",  # Repuestos y Accesorios (Refrigeración)
+                "MLC174295",  # Extractores y Purificadores
+                "MLC176937",  # Repuestos y Accesorios (Climatización)
+                "MLC85756",  # Accesorios (Tablet)
             ],
-            CELL: ['MLC1055'],  # Celulares y Smartphones
-            WEARABLE: ['MLC417704'],  # Smartwatches y accesorios
+            CELL: ["MLC1055"],  # Celulares y Smartphones
+            WEARABLE: ["MLC417704"],  # Smartwatches y accesorios
             VACUUM_CLEANER: [
-                'MLC1581',  # Pequeños electrodomésticos
+                "MLC1581",  # Pequeños electrodomésticos
             ],
             REFRIGERATOR: [
-                'MLC9456',  # Refrigeradores
+                "MLC9456",  # Refrigeradores
             ],
             AIR_CONDITIONER: [
-                'MLC29800',  # Aires Acondicionados
-                'MLC409431',  # Salud y Equipamiento Médico
+                "MLC29800",  # Aires Acondicionados
+                "MLC409431",  # Salud y Equipamiento Médico
             ],
             OVEN: [
-                'MLC30854',  # Hornos
-                'MLC30848',  # Microondas
+                "MLC30854",  # Hornos
+                "MLC30848",  # Microondas
             ],
-            DISH_WASHER: ['MLC174300'],  # Lavavajillas
+            DISH_WASHER: ["MLC174300"],  # Lavavajillas
             WASHING_MACHINE: [
-                'MLC178593',  # Lavadora-Secadoras
+                "MLC178593",  # Lavadora-Secadoras
             ],
             TABLET: [
-                'MLC82067',  # Tablets
+                "MLC82067",  # Tablets
             ],
             MONITOR: [
-                'MLC1655',  # Monitores y Accesorios
+                "MLC1655",  # Monitores y Accesorios
             ],
             PROJECTOR: [
-                'MLC9239',  # Proyectores y Telones
-                'MLC1657',  # Proyectores y Telones
+                "MLC9239",  # Proyectores y Telones
+                "MLC1657",  # Proyectores y Telones
             ],
-
         }
+
         session = session_with_proxy(extra_args)
+        session.headers["Authorization"] = f"Bearer {extra_args['access_token']}"
         product_urls = []
+
         for category_code in categories_codes[category]:
             product_urls.extend(
-                cls.get_products(session, category, category_code,
-                                 official_store_id=cls.official_store_id))
+                cls.get_products(
+                    session,
+                    category,
+                    category_code,
+                    official_store_id=cls.official_store_id,
+                )
+            )
 
         # Sanity check, verify that we are getting all products
         # all_products = cls.get_products(
@@ -96,7 +116,8 @@ class MercadoLibreSamsung(MercadoLibreChile):
         # only displays entries without a seller (not from marketplaces)
         # and we want to consider MercadoLibreSamsung for that.
         products = super().products_for_url(
-            url, category=category, extra_args=extra_args)
+            url, category=category, extra_args=extra_args
+        )
 
         filtered_products = []
 
