4d31cb278b89c837eaf0266db25ff1de4fd1178c
Vijay Khemlani <vj@solotodo.com>
2025-04-02 13:38:35 -0300

    * Updated Easy

--- storescraper/stores/easy.py
+++ storescraper/stores/easy.py
@@ -326,9 +326,9 @@ class Easy(Store):
                 variations_entries = " / ".join(
                     [item["itemSpecifications"][axis][0] for axis in variations_axis]
                 )
-                name = f"{item['name']} ({variations_entries})"
+                name = f"{response['brand']} {item['name']} ({variations_entries})"
             else:
-                name = item["name"]
+                name = f"{response['brand']} {item["name"]}"
 
             picture_urls = [img["imageUrl"] for img in item["images"]]
 
