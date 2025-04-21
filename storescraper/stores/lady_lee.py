6944a030fb6a2472e5ba64c8dba43bdc2fbf3074
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-04-21 11:55:26 -0400

    update LadyLee

--- storescraper/stores/lady_lee.py
+++ storescraper/stores/lady_lee.py
@@ -1,10 +1,11 @@
+from bs4 import BeautifulSoup
 from decimal import Decimal
 import json
 
 from storescraper.categories import TELEVISION
 from storescraper.product import Product
 from storescraper.store import Store
-from storescraper.utils import session_with_proxy, html_to_markdown, remove_words
+from storescraper.utils import cf_session_with_proxy, html_to_markdown
 
 
 class LadyLee(Store):
@@ -17,7 +18,7 @@ class LadyLee(Store):
     def discover_urls_for_category(cls, category, extra_args=None):
         url_extensions = [TELEVISION]
 
-        session = session_with_proxy(extra_args)
+        session = cf_session_with_proxy(extra_args)
         product_urls = []
 
         for local_category in url_extensions:
@@ -28,23 +29,20 @@ class LadyLee(Store):
                 if page > 10:
                     raise Exception("Page overflow")
 
-                url_webpage = (
-                    "https://api.c8gqzlqont-mantenimi1-p1-public.m"
-                    "odel-t.cc.commerce.ondemand.com/occ/v2/myshop-spa/produ"
-                    "cts/search?query=LG:relevance:brand:LG&pageSize=100&cur"
-                    "rentPoS=D001&currentPage={}".format(page)
-                )
+                url = f"https://ladylee.net/search?type=product&q=lg&page={page}"
+                print(url)
 
-                data = session.get(url_webpage).text
-                json_data = json.loads(data)["products"]
+                data = session.get(url)
+                soup = BeautifulSoup(data.text, "lxml")
+                products = soup.find_all("div", "product-item")
 
-                if len(json_data) == 0:
-                    if page == 0:
-                        raise Exception("Empty category: " + url_webpage)
+                if not products:
                     break
 
-                for product in json_data:
-                    product_urls.append("https://ladylee.net" + product["url"])
+                for product in products:
+                    product_urls.append(
+                        f"https://ladylee.net/{product.find('a')['href']}"
+                    )
 
                 page += 1
 
@@ -53,33 +51,27 @@ class LadyLee(Store):
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
-        session = session_with_proxy(extra_args)
-        sku = url.split("/")[4]
-        product_url = (
-            "https://api.c8gqzlqont-mantenimi1-p1-public.model-t.c"
-            "c.commerce.ondemand.com/occ/v2/myshop-spa/products/{}?fields=DE"
-            "FAULT,images(FULL,galleryIndex),ean&currentPoS=D001".format(sku)
-        )
-        response = session.get(product_url, allow_redirects=False)
+        session = cf_session_with_proxy(extra_args)
+        response = session.get(url)
+        soup = BeautifulSoup(response.text, "lxml")
+        product_data_tag = soup.find("script", {"type": "application/ld+json"})
 
-        if response.status_code == 400:
-            # Their own version of a 404 error
+        if not product_data_tag:
             return []
 
-        json_data = json.loads(response.text)
+        product_data = json.loads(product_data_tag.text)
+        name = product_data["name"]
 
-        assert sku == json_data["code"]
+        if "sku" not in product_data:
+            return []
 
-        description = json_data["description"]
-        name = json_data["name"]
-        price = Decimal(remove_words(json_data["price"]["formattedValue"], ["L.", ","]))
-
-        if json_data["availableForPickup"]:
-            stock = -1
-        else:
-            stock = 0
-
-        picture_urls = [json_data["images"][0]["url"]]
+        sku = product_data["sku"]
+        description = html_to_markdown(product_data["description"])
+        offer = product_data["offers"]
+        variant_id = offer["url"].split("?variant=")[-1]
+        price = Decimal(offer["price"])
+        stock = -1 if offer["availability"] == "http://schema.org/InStock" else 0
+        picture_urls = [product_data["image"].split("?")[0]]
 
         p = Product(
             name,
@@ -92,7 +84,7 @@ class LadyLee(Store):
             price,
             price,
             "HNL",
-            sku=sku,
+            sku=variant_id,
             picture_urls=picture_urls,
             description=description,
         )
