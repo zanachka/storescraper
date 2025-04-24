4f32e152c4f4810307f5584dbf0b8226551f7173
Vijay Khemlani <vj@solotodo.com>
2025-04-24 09:31:38 -0400

    * Updated Diunsa

--- storescraper/stores/diunsa.py
+++ storescraper/stores/diunsa.py
@@ -59,6 +59,10 @@ class Diunsa(Store):
         print(url)
         session = session_with_proxy(extra_args)
         response = session.get(url)
+
+        if response.status_code == 404:
+            return []
+
         soup = BeautifulSoup(response.text, "html5lib")
 
         product_data = json.loads(
