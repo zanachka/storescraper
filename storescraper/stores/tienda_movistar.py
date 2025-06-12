76be95f79f10f19b99089628282f141a5a2b3988
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-06-12 11:33:29 -0400

    update TiendaMovistar

--- storescraper/stores/tienda_movistar.py
+++ storescraper/stores/tienda_movistar.py
@@ -16,11 +16,23 @@ from storescraper.utils import remove_words
 
 class TiendaMovistar(Store):
     category_paths = [
-        ("Electronica > Comunicacion > Telefonia > Telefonos moviles", CELL),
-        ("Electronica > Ordenadores > Tablets", TABLET),
-        ("Electrónica > Audio > Equipo de sonido > Auriculares", HEADPHONES),
-        ("Electrónica > Video > Televisores", TELEVISION),
-        ("Electrónica > Audio > Equipo de sonido > Altavoces", STEREO_SYSTEM),
+        (
+            "celulares",
+            "Electronica > Comunicacion > Telefonia > Telefonos moviles",
+            CELL,
+        ),
+        ("tablets", "Electronica > Ordenadores > Tablets", TABLET),
+        (
+            "audifonos",
+            "Electrónica > Audio > Equipo de sonido > Auriculares",
+            HEADPHONES,
+        ),
+        ("smarthome", "Electrónica > Video > Televisores", TELEVISION),
+        (
+            "parlantes-bluetooth",
+            "Electrónica > Audio > Equipo de sonido > Altavoces",
+            STEREO_SYSTEM,
+        ),
     ]
 
     @classmethod
@@ -35,13 +47,14 @@ class TiendaMovistar(Store):
 
     @classmethod
     def discover_urls_for_category(cls, category, extra_args=None):
-        return [category]
+        url = cls.generate_discover_url_for_category(category)
+        return [url]
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         products = []
 
-        for category_path, local_category in cls.category_paths:
+        for _, category_path, local_category in cls.category_paths:
             if local_category != category:
                 continue
 
@@ -66,7 +79,7 @@ class TiendaMovistar(Store):
                         store=cls.__name__,
                         category=category,
                         url=entry["enlace"],
-                        discovery_url=entry["enlace"],
+                        discovery_url=cls.generate_discover_url_for_category(category),
                         key=sku,
                         stock=0 if entry["disponibilidad"] == "agotado" else -1,
                         normal_price=price,
@@ -84,3 +97,9 @@ class TiendaMovistar(Store):
                 )
 
         return products
+
+    @classmethod
+    def generate_discover_url_for_category(cls, category):
+        for url_path, _, local_category in cls.category_paths:
+            if local_category == category:
+                return f"https://catalogo.movistar.cl/tienda/{url_path}"
