347fb3a9ecbdaca770f8bae83bf45f4685852629
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-06-23 12:07:42 -0400

    update ZonaPortatil

--- storescraper/stores/zona_portatil.py
+++ storescraper/stores/zona_portatil.py
@@ -1,122 +1,134 @@
-import logging
-import re
 from decimal import Decimal
 
-from bs4 import BeautifulSoup
-
 from storescraper.categories import (
+    ACCESORIES,
     ALL_IN_ONE,
-    NOTEBOOK,
-    HEADPHONES,
-    VIDEO_GAME_CONSOLE,
-    GAMING_CHAIR,
+    CELL,
     COMPUTER_CASE,
-    RAM,
-    MOTHERBOARD,
-    PROCESSOR,
-    VIDEO_CARD,
-    STORAGE_DRIVE,
-    SOLID_STATE_DRIVE,
+    CPU_COOLER,
     EXTERNAL_STORAGE_DRIVE,
+    GAMING_CHAIR,
+    HEADPHONES,
+    KEYBOARD,
+    KEYBOARD_MOUSE_COMBO,
     MEMORY_CARD,
     MONITOR,
-    PRINTER,
-    CPU_COOLER,
-    POWER_SUPPLY,
-    KEYBOARD_MOUSE_COMBO,
-    CASE_FAN,
-    UPS,
+    MOTHERBOARD,
     MOUSE,
+    NOTEBOOK,
+    OVEN,
+    POWER_SUPPLY,
+    PRINTER,
     PRINTER_SUPPLY,
+    PROCESSOR,
+    PROJECTOR,
+    RAM,
+    SOLID_STATE_DRIVE,
+    SPACE_HEATER,
+    STEREO_SYSTEM,
+    STORAGE_DRIVE,
     TABLET,
+    TELEVISION,
+    UPS,
+    USB_FLASH_DRIVE,
+    VIDEO_CARD,
+    WEARABLE,
 )
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
-from storescraper.utils import html_to_markdown, session_with_proxy, remove_words
+from storescraper.utils import session_with_proxy
 
 
 class ZonaPortatil(StoreWithUrlExtensions):
     url_extensions = [
-        ["teclado-mouse", KEYBOARD_MOUSE_COMBO],
-        ["fuente-de-poder", POWER_SUPPLY],
-        ["memoria-notebook", RAM],
-        ["memoria-pc", RAM],
-        ["placa-madre", MOTHERBOARD],
-        ["procesador", PROCESSOR],
-        ["tarjeta-de-video", VIDEO_CARD],
-        ["ventilador", CASE_FAN],
-        ["discos-duros-externos", EXTERNAL_STORAGE_DRIVE],
-        ["disco-duro-servidor", STORAGE_DRIVE],
-        ["disco-duro-gamer", STORAGE_DRIVE],
-        ["disco-duro-ssd", SOLID_STATE_DRIVE],
-        ["memorias-flash", MEMORY_CARD],
-        ["aio-todo-en-uno", ALL_IN_ONE],
-        ["notebook", NOTEBOOK],
-        ["tablets", TABLET],
-        ["ups", UPS],
-        ["impresoras", PRINTER],
-        ["suministros", PRINTER_SUPPLY],
-        ["monitores-monitores", MONITOR],
-        ["impresoras-open-box", PRINTER],
-        ["notebook-partes-y-piezas-open-box", NOTEBOOK],
-        ["memoria-notebook-partes-y-piezas-open-box", RAM],
-        ["memoria-pc-partes-y-piezas-open-box", RAM],
-        ["notebook-computadores-open-box", RAM],
-        ["disco-duro-externo-almacenamiento-open-box", EXTERNAL_STORAGE_DRIVE],
-        ["disco-duro-ssd-almacenamiento-open-box", SOLID_STATE_DRIVE],
-        ["ups-y-energia-ups-y-energia-ups-y-energia", UPS],
-        ["mouse", MOUSE],
-        ["teclado-mouse-accesorios-zona-gamer", KEYBOARD_MOUSE_COMBO],
-        ["silla-gamer", GAMING_CHAIR],
-        ["ventilador-accesorios-zona-gamer", CPU_COOLER],
-        ["gabinetes-gabinetes", COMPUTER_CASE],
-        ["monitores-monitores-zona-gamer", MONITOR],
-        ["refrigeracion-refrigeracion", CPU_COOLER],
-        ["audifonos-audio-zona-gamer", HEADPHONES],
-        ["audifonos-audifonos", HEADPHONES],
-        ["memorias-notebook-memorias-notebook", RAM],
-        ["tarjetas-graficas-tarjetas-graficas", VIDEO_CARD],
-        ["tarjeta-de-video-partes-y-piezas-zona-gamer", VIDEO_CARD],
-        ["placa-madre-partes-y-piezas-zona-gamer", MOTHERBOARD],
-        ["consolas-consolas", VIDEO_GAME_CONSOLE],
-        ["memorias-pc-memorias-pc", RAM],
-        ["placas-madres-placas-madres", MOTHERBOARD],
-        ["fuente-de-poder-fuente-de-poder-zona-gamer", POWER_SUPPLY],
-        ["procesadores-procesadores", PROCESSOR],
+        ("0204ce76-e551-4b0f-9fc0-9695abb53b9b", HEADPHONES),
+        ("32e0920d-c46e-4a83-b84d-6cc98d56e998", STEREO_SYSTEM),
+        ("d40bbab2-7885-43eb-8f08-96249e3d69a0", ACCESORIES),
+        ("b20bdfb9-eebf-4ca3-ba5f-07db3bff495e", RAM),
+        ("ae821fc9-31c2-44d7-affc-089e88e48b4b", RAM),
+        ("de901580-3e10-48df-bf94-db2340977d8a", RAM),
+        ("c81a39a0-a93f-4fb6-9e4b-92d6a254ca2a", USB_FLASH_DRIVE),
+        ("e1df34cc-6d2c-4a38-b798-3d725444e12e", MEMORY_CARD),
+        ("e769bc5d-07b9-4a62-bbba-5ccdf8edbcce", NOTEBOOK),
+        ("f4fd9a6b-194f-4e27-b0a9-cecf84243a73", ALL_IN_ONE),
+        ("ab5ced88-d056-4d26-ba84-ac2782912e6a", TABLET),
+        ("6b5ed6b5-7849-4b53-a8cc-68b3e7a41f5d", NOTEBOOK),
+        ("ccff56c2-3b69-4059-a527-80ad36a4afcd", MONITOR),
+        ("e7a51952-f018-493e-82c9-db5f2423ce6d", TELEVISION),
+        ("25dade18-1879-4f15-92dc-8609b2edbade", PROJECTOR),
+        ("6d3d9155-b9cc-42d0-8a7f-cae889b1e54a", SOLID_STATE_DRIVE),
+        ("4401c79d-3ece-4243-94c1-060ec6892740", EXTERNAL_STORAGE_DRIVE),
+        ("8c92973e-96f7-4316-af18-184988ef5743", STORAGE_DRIVE),
+        ("9504e13e-8f9c-406f-9b98-dbb357168b53", EXTERNAL_STORAGE_DRIVE),
+        ("a7aaf0f1-10b3-4bde-97bc-1545e2bc419d", MOUSE),
+        ("30785fe1-b336-420b-9d2a-a8b25f16a83c", HEADPHONES),
+        ("22090844-46ac-4147-85e1-24be056981ca", TABLET),
+        ("7f8c2041-fe5d-4db8-a836-678fac22e526", STEREO_SYSTEM),
+        ("a04b14fd-50fd-4dd5-b9bb-9ebd281d6fc6", KEYBOARD_MOUSE_COMBO),
+        ("f3860ccc-b305-4839-b052-08cb4a4e6097", KEYBOARD),
+        ("202fa576-5095-4b8e-99e9-845d1c1ff5d7", PROCESSOR),
+        ("84e7d4cd-92f2-4bd7-98eb-c1492d3c3755", MOTHERBOARD),
+        ("4e5e9ad6-c9f8-4fb8-a2d7-a1b3a0a813b3", CPU_COOLER),
+        ("567972cb-02fc-49c6-9547-1fae72ee70dd", POWER_SUPPLY),
+        ("2b9c675d-fdcc-43a0-9df6-78c81a1233a5", VIDEO_CARD),
+        ("55adcb0f-ef21-4490-b2c5-dd060837ecd1", COMPUTER_CASE),
+        ("f3339873-5aca-4bd2-8ecb-46fe43483acb", UPS),
+        ("e6829f33-690e-41e5-9fc8-9079e9fd4548", STEREO_SYSTEM),
+        ("62d06711-8c68-4e43-b4f2-ed2df5fa3e1e", HEADPHONES),
+        ("fa64c581-aa58-4aa2-a8b1-bd970c21abd5", STEREO_SYSTEM),
+        ("e98f4206-6aed-45b6-89e0-dfb48b8c7d44", OVEN),
+        ("c0a5895a-c077-4636-b00a-689bbf7f4b3c", SPACE_HEATER),
+        ("62ea0ecf-dd5b-4343-a649-e66738361fe0", STORAGE_DRIVE),
+        ("b9a1216b-5577-4b73-9ec2-32830216e84a", EXTERNAL_STORAGE_DRIVE),
+        ("a7897f04-cd47-458b-b88d-bf1b43e00abd", MOUSE),
+        ("1c582563-fbfe-4e45-868d-752302919f7f", KEYBOARD),
+        ("656f4b11-a2c9-4eeb-884d-8237ba5c9408", KEYBOARD_MOUSE_COMBO),
+        ("833806d1-78e1-4866-a381-7f1c10f599e4", HEADPHONES),
+        ("cb3845c7-b47c-4b37-9ad9-663dab30b24e", COMPUTER_CASE),
+        ("e66f1ac4-db92-495a-b216-d51a983d0f11", VIDEO_CARD),
+        ("ab291f65-4318-4f47-926b-52f07e04e73d", PROCESSOR),
+        ("d97ba9ba-1add-4d53-9998-1021e5d2b4e3", CPU_COOLER),
+        ("add751d5-fbea-4d44-82f1-b9cc447e92f7", MOTHERBOARD),
+        ("2a0423af-75cd-4619-b63b-6ef2889756c2", POWER_SUPPLY),
+        ("bc12b3dc-40b3-4ee0-97d0-3c5de384fb16", NOTEBOOK),
+        ("316e5ad0-b951-4b22-b5fc-87fcc430b411", RAM),
+        ("51ab965d-8184-4886-b967-8c71f4d97d20", RAM),
+        ("ae40b2e1-5183-43b6-8000-38efa032d1e3", UPS),
+        ("6d17f312-4c68-4775-a05c-d4e546e81740", RAM),
+        ("086fec30-b268-4eb5-8cf6-9072889b6a83", TABLET),
+        ("7e983fb2-0fac-4137-a08b-3b205d1b28ea", CELL),
+        ("b2be9aab-e4f3-4cd6-80f9-bca6e3e25e8b", WEARABLE),
+        ("15036c1a-7c17-44ca-80ae-852d5f462ce3", PRINTER),
+        ("32636625-bdaa-4c18-8db7-947856212557", PRINTER),
+        ("fee80a53-0298-4f28-bc82-04e5e1b22159", PRINTER),
+        ("ded2313f-5773-47a6-8ccf-8b73acb54e5f", PRINTER),
+        ("9d17db5b-75f1-465b-9b47-ba48d80fa4ce", PRINTER_SUPPLY),
+        ("37d9008b-8458-4462-a185-e7707fae0605", GAMING_CHAIR),
     ]
 
     @classmethod
     def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         session = session_with_proxy(extra_args)
         product_urls = []
-        page = 1
+        endpoint = (
+            "https://api-zonaprod.zonaportatil.cl/product/get-all-active-products"
+        )
+        print(url_extension)
+        response = session.get(endpoint)
 
-        while True:
-            if page > 10:
-                raise Exception("page overflow: " + url_extension)
+        if response.status_code != 200:
+            raise Exception(f"Invalid category: {url_extension}")
 
-            url_webpage = "https://zonaportatil.cl/categoria/{}/page/{}/".format(
-                url_extension, page
+        products = [
+            p
+            for p in response.json()["products"]["data"]
+            if p["subcategory"]["id"] == url_extension
+        ]
+
+        for product in products:
+            product_urls.append(
+                f"https://www.zonaportatil.cl/producto_completo.php?idProduct={product['id']}"
             )
-            print(url_webpage)
-            response = session.get(url_webpage)
-
-            if response.status_code == 404 and page == 1:
-                raise Exception("Invalid category: " + url_extension)
-
-            soup = BeautifulSoup(response.text, "lxml")
-            product_containers = soup.findAll("article", "product")
-
-            if not product_containers:
-                if page == 1:
-                    logging.warning("Empty category")
-                break
-
-            for container in product_containers:
-                product_url = container.find("a")["href"]
-                product_urls.append(product_url)
-
-            page += 1
 
         return product_urls
 
@@ -124,57 +136,26 @@ class ZonaPortatil(StoreWithUrlExtensions):
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = session_with_proxy(extra_args)
-        response = session.get(url)
-        soup = BeautifulSoup(response.text, "html.parser")
-        name = soup.find("p", "product_title").text.strip()
-        alternate_url = soup.find(
-            "link", {"rel": "alternate", "type": "application/json"}
-        )["href"]
-        key_match = re.search(r"/product/(\d+)", alternate_url)
-        key = key_match.groups()[0]
+        key = url.split("?idProduct=")[-1]
+        endpoint = f"https://api-zonaprod.zonaportatil.cl/product/{key}"
+        response = session.get(endpoint)
 
-        qty_input = soup.find("input", "input-text qty text")
+        if response.status_code != 200:
+            return []
 
-        if qty_input:
-            if qty_input["max"]:
-                stock = int(qty_input["max"])
-            else:
-                stock = -1
-        else:
-            if soup.find("button", "single_add_to_cart_button"):
-                stock = 1
-            else:
-                stock = 0
-
-        price_containers = soup.find("p", "price")
-        normal_price = Decimal(
-            remove_words(price_containers.find("span", "woocommerce-Price-amount").text)
+        product = response.json()["product"]["data"]
+        name = product["name"]
+        stock = product["stock"]
+        normal_price = Decimal(product["normal_prince"])
+        offer_price = (normal_price * Decimal(0.95)).quantize(0)
+        condition = (
+            "https://schema.org/OpenBoxCondition"
+            if "OPEN" in name.upper()
+            else "https://schema.org/NewCondition"
         )
-
-        offer_price_tag = price_containers.find("p", "price-transferencia")
-
-        if not offer_price_tag:
-            raise Exception(response.text)
-
-        offer_price = Decimal(remove_words(offer_price_tag.find("bdi").text))
-
-        if offer_price > normal_price:
-            offer_price = normal_price
-
-        sku = re.search(r"Part Number: (.+?)<", response.text).groups()[0]
-        picture_urls = [
-            x["href"]
-            for x in soup.find("div", "woocommerce-product-gallery__wrapper").findAll(
-                "a"
-            )
-        ]
-
-        if "OPEN" in name.upper():
-            condition = "https://schema.org/OpenBoxCondition"
-        else:
-            condition = "https://schema.org/NewCondition"
-
-        description = html_to_markdown(soup.find("div", {"id": "tab-description"}).text)
+        sku = product["sku"]
+        picture_urls = [img["path"] for img in product["images"]]
+        description = product["broadDescription"]
 
         p = Product(
             name,
