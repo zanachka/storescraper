fc6281df2dc3c2b9cfd593b5f14c0b11bb4455ba
Vijay Khemlani <vj@solotodo.com>
2025-07-11 10:55:37 -0400

    * Updated GamingX

--- storescraper/stores/gaming_x.py
+++ storescraper/stores/gaming_x.py
@@ -3,62 +3,60 @@ import logging
 import json
 from storescraper.product import Product
 from bs4 import BeautifulSoup
-from storescraper.categories import NOTEBOOK, MONITOR, CPU_COOLER, VIDEO_CARD
+from storescraper.categories import (
+    NOTEBOOK,
+    MONITOR,
+    CPU_COOLER,
+    VIDEO_CARD,
+    MOTHERBOARD,
+    PROCESSOR,
+    RAM,
+)
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
 from storescraper.utils import cf_session_with_proxy, html_to_markdown, remove_words
 
 
 class GamingX(StoreWithUrlExtensions):
-    @classmethod
-    def categories(cls):
-        return [NOTEBOOK, MONITOR, CPU_COOLER, VIDEO_CARD]
+    url_extensions = [
+        ("notebooks", NOTEBOOK),
+        ("monitores", MONITOR),
+        ("refrigeracion", CPU_COOLER),
+        ("tarjetas-de-video", VIDEO_CARD),
+        ("placas-madres", MOTHERBOARD),
+        ("procesadores", PROCESSOR),
+        ("memoria-ram", RAM),
+    ]
 
     @classmethod
-    def discover_urls_for_category(cls, category, extra_args=None):
-        url_extensions = [
-            ("notebooks", NOTEBOOK),
-            ("monitores", MONITOR),
-            ("refrigeracion", CPU_COOLER),
-            ("tarjetas-de-video", VIDEO_CARD),
-        ]
-
+    def discover_urls_for_url_extension(cls, url_extension, extra_args=None):
         session = cf_session_with_proxy(extra_args)
-        product_urls = []
+        page = 1
 
-        for url_extension, local_category in url_extensions:
-            if local_category != category:
-                continue
+        while True:
+            if page > 10:
+                raise Exception(f"Page overflow: {url_extension}")
 
-            page = 1
+            url_webpage = f"https://www.gamingx.cl/{url_extension}/page/{page}"
+            print(url_webpage)
+            data = session.get(url_webpage)
+            soup = BeautifulSoup(data.text, "lxml")
+            product_containers = soup.findAll("div", "product-item")
 
-            while True:
-                if page > 10:
-                    raise Exception(f"Page overflow: {url_extension}")
+            if not product_containers:
+                if page == 1:
+                    logging.warning(f"Empty category: {url_extension}")
+                break
 
-                url_webpage = f"https://www.gamingx.cl/{url_extension}/page/{page}"
-                print(url_webpage)
-                data = session.get(url_webpage)
-                soup = BeautifulSoup(data.text, "lxml")
-                product_containers = soup.findAll("div", "product-item")
-
-                if not product_containers:
-                    if page == 1:
-                        logging.warning(f"Empty category: {url_extension}")
-                    break
-
-                for container in product_containers:
-                    product_url = container.find("a")["href"]
-                    product_urls.append(product_url)
-                page += 1
-
-        return product_urls
+            for container in product_containers:
+                product_url = container.find("a")["href"]
+                yield product_url
+            page += 1
 
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
         session = cf_session_with_proxy(extra_args)
         soup = BeautifulSoup(session.get(url).text, "lxml")
-        products = []
         variations = soup.find("div", {"id": "single-product"})
         name = soup.find("h1", "js-product-name").text.strip()
 
@@ -99,7 +97,4 @@ class GamingX(StoreWithUrlExtensions):
                 description=description,
                 picture_urls=picture_urls,
             )
-
-            products.append(p)
-
-        return products
+            yield p
