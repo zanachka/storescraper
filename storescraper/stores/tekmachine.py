628e8a4a4c76877dad7ade574ec87ae9e62a7232
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-07-14 10:44:23 -0400

    update Tekmachine

--- storescraper/stores/tekmachine.py
+++ storescraper/stores/tekmachine.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 from storescraper.categories import *
 from storescraper.product import Product
 from storescraper.store_with_url_extensions import StoreWithUrlExtensions
-from storescraper.utils import html_to_markdown, session_with_proxy, remove_words
+from storescraper.utils import html_to_markdown, cf_session_with_proxy, remove_words
 
 
 class Tekmachine(StoreWithUrlExtensions):
@@ -28,7 +28,7 @@ class Tekmachine(StoreWithUrlExtensions):
 
     @classmethod
     def discover_urls_for_url_extension(cls, url_extension, extra_args):
-        session = session_with_proxy(extra_args)
+        session = cf_session_with_proxy(extra_args)
         product_urls = []
         page = 1
 
@@ -57,10 +57,10 @@ class Tekmachine(StoreWithUrlExtensions):
     @classmethod
     def products_for_url(cls, url, category=None, extra_args=None):
         print(url)
-        session = session_with_proxy(extra_args)
+        session = cf_session_with_proxy(extra_args)
         response = session.get(url)
         soup = BeautifulSoup(response.text, "html5lib")
-        name = soup.find("h1", "product_title").text.strip()
+        name = soup.find("span", "breadcrumb-last").text.strip()
         key = soup.find("link", {"rel": "shortlink"})["href"].split("p=")[1]
         sku_tag = soup.find("span", "sku")
         sku = sku_tag.text.strip()[:45] if sku_tag else None
