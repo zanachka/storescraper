3fe554a52be95a2953cde7a2c894437f16ceb04c
Tomás Díaz Toro <tomasdiaztoro@gmail.com>
2025-08-12 11:08:51 -0400

    update NotebooksYa

--- storescraper/stores/notebooks_ya.py
+++ storescraper/stores/notebooks_ya.py
@@ -201,6 +201,10 @@ class NotebooksYa(StoreWithUrlExtensions):
         data = json.loads(gtag_text)
         offer_price = Decimal(data["products"][str(key)]["price"])
         normal_price = Decimal(math.ceil(offer_price * Decimal(1.03)))
+
+        if normal_price > Decimal("100000000") or offer_price > Decimal("100000000"):
+            return []
+
         sku = str(product_data["sku"])
         picture_urls = [a["href"] for a in soup.findAll("a", "swiper-slide-imglink")]
         description = html_to_markdown(str(soup.find("div", {"id": "tab-description"})))
